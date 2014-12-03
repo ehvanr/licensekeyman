56f7a1659d78dd384860ef4798f40284d1e6acd1
Evan Reichard <evan@evanreichard.com>
2014-12-02 21:08:35 -0500

    Client Registration Changes
    Added some more functionality in the client API

--- clientLogic.js
+++ clientLogic.js
@@ -1,23 +1,14 @@
 var dbConnection = require("./dbConnector.js");
  
 module.exports = {
-	executeTestQuery: function (cb){
-		// Execute the query
-		var query = dbConnection.connection.query('select * from applicationkeys;', null, function(err, result) {
-			cb(result);
-		});
-		
-	},
 	
-	checkLicense: function (licensekey, email, cb){
+    checkLicense: function (licensekey, email, appID, cb){
 		// Execute the query
-		var query = dbConnection.connection.query('select * from applicationkeys inner join users on users.userID = applicationkeys.userID where `Key` = ? and users.UserEmail = ?;', [licensekey, email], function(err, result) {
-			
-			// If no value returned theres no record, if value returned: if not activated then activate, if activated check if expired, if not expired allow
+		var query = dbConnection.connection.query('select * from applicationkeys inner join users on users.userID = applicationkeys.userID where `Key` = ? and users.UserEmail = ? and applicationkeys.applicationID = ?;', [licensekey, email, appID], function(err, result) {
 			
 			// Check if there's a record
-			if(result.length < 1){
-				cb({"ERROR":"No Records","ERROR_CODE":1000});
+			if(result === undefined || result.length < 1){
+				cb({"ERROR":"No Records","CODE":1000});
 			}else{
 				// Check if the key is in use
 				if(result[0].InUse === 1){
@@ -25,24 +16,54 @@ module.exports = {
 					// Check if email matches the returned value
 					if(result[0].UserEmail == email){
 						
-						// Check if we are still within normal date range
-						if(true){
-							// We are within range
-							cb(result);
+                        var Today = new Date();
+						
+                        // Check if we are still within normal date range
+						if((result[0].Issued < Today) && (result[0].Expires > Today)){
+							cb({"SUCCESS":"License Validated","CODE":2000});
 						}else{
 							// We are not within range
-							cb({"ERROR":"Licence Expired","ERROR_CODE":1003});
+							cb({"ERROR":"Licence not active","CODE":1003});
 						}
 					}else{
 						// Invalid email tried to register, audit here
-						cb({"ERROR":"Invalid credentials","ERROR_CODE":1002});
+						cb({"ERROR":"Invalid credentials","CODE":1002});
 					}
 				}else{
 					// Respond with license key is not registered, please register at different API endpoint
-					cb({"ERROR":"License Key is not registerd.","ERROR_CODE":1001});
+					cb({"ERROR":"License Key is not registerd.","CODE":1001});
 				}
 			}
 		});
 		
 	},
+   
+
+    registerApplication: function(licensekey, email, appID, cb){
+		
+        checkLicense(licensekey, email, appID, function(data){
+                
+            if(data.CODE === 1001){
+                // License key is not registered, so we register it here
+                
+                dbConnection.connection.query("update "); 
+            
+                cb(data);
+            }else{
+                cb({"ERROR":"","CODE":""});
+            }
+                
+        });
+    },
+
+    registerUser: function(user, email, cb){
+	    dbConnection.connection.query('insert into users (UserName, UserEmail) VALUES (?, ?);', [user, email], function(err, result){
+            console.log(err);
+            if(err){
+                cb({"ERROR":"User Added Failed","CODE":1004});    
+            }else{
+                cb({"SUCCESS":"User Added Successfully","CODE":2000});    
+            }
+        });	
+    }
 };
--- clientRoutes.js
+++ clientRoutes.js
@@ -2,16 +2,12 @@ var clientLogic = require('./clientLogic.js');
 
 module.exports = function(router){
 	
-	router.get('/', function(req, res) {
-        res.json(req.body);
-	})
-	
-	.post('/checklicense', function(req, res) {
+	router.post('/check_license', function(req, res) {
 		// ApplicationID
 		//res.json(req.body.licensekey)
 	
-		if(req.body.licensekey && req.body.email){	
-			clientLogic.checkLicense(req.body.licensekey, req.body.email, function(data){
+		if(req.body.licensekey && req.body.email && req.body.appID){	
+			clientLogic.checkLicense(req.body.licensekey, req.body.email, req.body.appID, function(data){
 				res.json(data);	
 			});
 		}else{
@@ -19,7 +15,25 @@ module.exports = function(router){
 		}
 	})
 	
-	.post('/register', function(req, res) {
-		res.json(req.body.test)
-	});	
+	.post('/register_application', function(req, res) {
+		
+        if(req.body.licensekey && req.body.email && req.body.appID){	
+			clientLogic.checkLicense(req.body.licensekey, req.body.email, req.body.appID, function(data){
+				res.json(data);	
+			});
+		}else{
+			res.json(req.body);
+		}
+	})
+
+    .post('/register_user', function(req, res) {
+		
+        if(req.body.username && req.body.email){	
+			clientLogic.registerUser(req.body.username, req.body.email, function(data){
+				res.json(data);	
+			});
+		}else{
+			res.json(req.body);
+		}
+	});
 }
